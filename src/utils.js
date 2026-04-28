35dc51a03cad1906072c72e23a26c5409a53bdaa
Vuk <vuk@Vuks-MacBook-Pro.local>
2026-04-28 21:40:07 +0200

    fix: base path /BookR/

--- src/utils.js
+++
@@ -1,3 +0,0 @@
-export const sv = (k, v) => { try { localStorage.setItem(k, JSON.stringify(v)) } catch (_) {} }
-export const ld = (k, d) => { try { const v = localStorage.getItem(k); return v !== null ? JSON.parse(v) : d } catch (_) { return d } }
-export const x = s => String(s).replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;')
